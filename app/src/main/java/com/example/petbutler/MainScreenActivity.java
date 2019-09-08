0a33f384bf212e9536e8ed04db4dae23889dc123
Rafael <rafaritz@gmail.com>
2019-09-08 19:22:49 -0300

    MainScreenActivity created

---
+++ app/src/main/java/com/example/petbutler/MainScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.petbutler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
