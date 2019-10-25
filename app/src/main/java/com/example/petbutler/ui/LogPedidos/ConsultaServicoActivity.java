d9087484a7fe074992cde8c9a8852ef7b75e026c
lucas-medeiros <lucascarmed@gmail.com>
2019-10-25 17:26:39 -0300

    Add Classes de Projeto e tela de consultar serviço

---
+++ app/src/main/java/com/example/petbutler/ui/LogPedidos/ConsultaServicoActivity.java
@@ -0,0 +1,20 @@
+package com.example.petbutler.ui.LogPedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petbutler.R;
+
+public class ConsultaServicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_servico);
+
+        getSupportActionBar().setTitle("Consultar Serviço");
+
+
+    }
+}
