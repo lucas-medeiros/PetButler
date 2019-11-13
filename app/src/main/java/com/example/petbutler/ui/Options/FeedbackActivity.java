ad8a2dc4d2bb40eae1ddf99b7cae95d217d609dd
lucas-medeiros <lucascarmed@gmail.com>
2019-11-12 22:53:58 -0300

    tela de add cartão (cliente e add conta (butler) sistema de avaliação dentro do app

--- app/src/main/java/com/example/petbutler/ui/Options/FeedbackActivity.java
+++ app/src/main/java/com/example/petbutler/ui/Options/FeedbackActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,20 +21,61 @@ public class FeedbackActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Relatar um problema");
 
-        TextView tvFeedback = findViewById(R.id.tv_feedback);
+        final TextView tvFeedback = findViewById(R.id.tv_feedback);
         final EditText etFeedback = findViewById(R.id.et_feedback);
+        final TextView tvAvaliacao = findViewById(R.id.tv_avaliacao);
         Button bEnviarFeedback = findViewById(R.id.b_enviarFeedback);
+        final RatingBar ratingBar_AvaliacaoApp = findViewById(R.id.ratingBar_avaliacaoApp);
+        final TextView tvAvaliacaoEscala = findViewById(R.id.tv_AvaliacaoEscala);
 
-        final String text = "Esta com dificuldades?\nSe sinta a vontade para nos enviar dúvidas ou reportar problemas do aplicativo\n" +
-                "\nObrigado por nos ajudar a melhorar!";
-        tvFeedback.setText(text);
+        final String text1 = "Está com dificuldades?\nSe sinta a vontade para nos enviar dúvidas ou reportar problemas do aplicativo\n" +
+                "\nObrigado por nos ajudar a melhorar!", text2 = "Você também avaliar sua experiência com o aplicativo:";
+        tvFeedback.setText(text1);
+        tvAvaliacao.setText(text2);
+        etFeedback.setLines(3);
+        ratingBar_AvaliacaoApp.setRating(5);
+        tvAvaliacaoEscala.setText("Incrível, adorei!");
+
+        ratingBar_AvaliacaoApp.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                tvAvaliacaoEscala.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        tvAvaliacaoEscala.setText("Muito ruim");
+                        break;
+                    case 2:
+                        tvAvaliacaoEscala.setText("Precisa de melhorias");
+                        break;
+                    case 3:
+                        tvAvaliacaoEscala.setText("Boa");
+                        break;
+                    case 4:
+                        tvAvaliacaoEscala.setText("Ótima");
+                        break;
+                    case 5:
+                        tvAvaliacaoEscala.setText("Incrível, adorei!");
+                        break;
+                    default:
+                        tvAvaliacaoEscala.setText("");
+                        break;
+                }
+            }
+        });
 
         bEnviarFeedback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Comentário enviado com sucesso\nObrigado!",Toast.LENGTH_SHORT).show();
                 String feedback = etFeedback.getText().toString();
+                int nota = ratingBar_AvaliacaoApp.getNumStars();
                 //salvar feedback no BD
+
+
+                etFeedback.setText("");
+                ratingBar_AvaliacaoApp.setRating(5);
+                tvAvaliacaoEscala.setText("Incrível, adorei!");
+
             }
         });
 
