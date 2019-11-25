affecb1d7127c3e2093dbc2d4e1b36fd89b67840
lucas-medeiros <lucascarmed@gmail.com>
2019-11-25 18:28:02 -0300

    Notas de atualização: Botões personalizados tela de editar pet, add cartão, add conta sistema de avaliação dentro do app tela de avaliação cliente + pet e butler imagem de background

--- app/src/main/java/com/example/petbutler/ui/EditarPerfil/EditarPetActivity.java
+++ app/src/main/java/com/example/petbutler/ui/EditarPerfil/EditarPetActivity.java
@@ -67,7 +67,7 @@ public class EditarPetActivity extends AppCompatActivity implements AdapterView.
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(),text, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(adapterView.getContext(),text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
