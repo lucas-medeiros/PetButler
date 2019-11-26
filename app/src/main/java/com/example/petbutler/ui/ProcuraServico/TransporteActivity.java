ec2c274745e3fe83fcbac227cae6ec7df6511471
Rafael <rafaritz@gmail.com>
2019-11-25 21:04:52 -0300

    Mudança do transporte

--- app/src/main/java/com/example/petbutler/ui/ProcuraServico/TransporteActivity.java
+++ app/src/main/java/com/example/petbutler/ui/ProcuraServico/TransporteActivity.java
@@ -1,66 +1,96 @@
 package com.example.petbutler.ui.ProcuraServico;
 
 /*In progress*/
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.petbutler.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.petbutler.ui.Classes.Animal.Animal;
+import com.example.petbutler.ui.Classes.Pessoa.Butler;
+import com.example.petbutler.ui.Classes.Pessoa.Cliente;
+import com.example.petbutler.ui.Classes.Pessoa.Telefone;
+import com.example.petbutler.ui.Classes.Servicos.Endereco;
+import com.example.petbutler.ui.Classes.Servicos.Transporte;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
-public class TransporteActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+public class TransporteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transporte);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        getSupportActionBar().setTitle("Pesquisar entregador");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final EditText etEnderecoTransporte = findViewById(R.id.editTextEnderecoTransporte);
+        final EditText etTempoTransporte = findViewById(R.id.editTextTempoTransporte);
+        final RadioGroup etRadioGroupTransporte = findViewById(R.id.radioGroupPets);
+        final RadioButton radioButtonCachorroTransporte = findViewById(R.id.radioButtonCachorroTransporte);
+        final RadioButton radioButtonPassaroTransporte = findViewById(R.id.radioButtonPassaroTransporte);
+        final RadioButton radioButtonGatoTransporte = findViewById(R.id.radioButtonGatoTransporte);
+        final ListView ListViewTransporte = findViewById(R.id.transporteList);
+        final Button buttonConfirmaTransporte = findViewById(R.id.buttonConfirmTransporte);
+
+        final ArrayList<String> a1transporte = new ArrayList<>();
+        String transportador1 = "Transportador 1", transportador2 = "Transportador 1", transportador3 = "Transportador 1";
+        a1transporte.add(transportador1);
+        a1transporte.add(transportador2);
+        a1transporte.add(transportador3);
+
+        Endereco endereco = new Endereco("80045355",330,null, "PR","Curitiba",null);
+        GregorianCalendar data = new GregorianCalendar(1998,12,27);
+        Animal ghost = new Animal("Ghost", "matador de white-walers", "Cachorro", "48.3", "grande",5.0,data,null);
+        Cliente JonSnow = new Cliente("Jon Snow","jonsnow","winter","000000000", "jonsnow@nigthswatch.com",
+                "Lord Commander of the Night's Watch",4.6,null,new Telefone("041","123456789"));
+        Butler Jaime = new Butler("Jaime Lannister","jaime","lovemysister","000000001", "ringslayer@lannister.com",
+                "Kingslayer",3.7,null,null);
+        final Transporte trans = new Transporte(0,data,4.8,5.0,4.9,endereco,"não concluído",JonSnow,Jaime,ghost);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, a1transporte);
+        ListViewTransporte.setAdapter(adapter);
+
+        ListViewTransporte.setVisibility(View.INVISIBLE);
+
+        buttonConfirmaTransporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(etRadioGroupTransporte.getCheckedRadioButtonId() == -1 || etEnderecoTransporte.getText().toString().isEmpty() ||
+                        etTempoTransporte.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Pesquisa concluída", Toast.LENGTH_SHORT).show();
+                    ListViewTransporte.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        ListViewTransporte.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
+                    Intent intentConsultaTransporte = new Intent(getApplicationContext(),ConsultaTransporteActivity.class);
+                    intentConsultaTransporte.putExtra("HOTEL_ID",trans.getId());
+                    startActivity(intentConsultaTransporte);
+                }
+            }
+        });
+
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-
-        final ArrayList<LatLng> altransporte = new ArrayList<>();
-
-        LatLng palladium = new LatLng(-25.4778215, -49.291088);
-        LatLng curitiba = new LatLng(-25.4409947, -49.2795614);
-        LatLng barigui = new LatLng(-25.4352014, -49.3186704);
-        LatLng angeloni = new LatLng(-25.4544463, -49.2904308);
-        altransporte.add(palladium);
-        altransporte.add(curitiba);
-        altransporte.add(barigui);
-        altransporte.add(angeloni);
-
-        mMap.addMarker(new MarkerOptions().position(altransporte.get(0)).title("Marker in Shopping Palladium"));
-        mMap.addMarker(new MarkerOptions().position(altransporte.get(1)).title("Marker in Shopping Curitiba"));
-        mMap.addMarker(new MarkerOptions().position(altransporte.get(2)).title("Marker in Shopping Barigui"));
-        mMap.addMarker(new MarkerOptions().position(altransporte.get(3)).title("Marker in Angeloni"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(altransporte.get(0)));
-    }
 }
